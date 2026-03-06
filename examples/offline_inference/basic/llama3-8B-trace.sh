cd22309c0e16c07e4fafc97c242a1e9b2ecb58ea
micropuma <micropuma@163.com>
2026-03-06 23:03:46 +0800

    [chore] add torch.compile trace examples

---
+++ examples/offline_inference/basic/llama3-8B-trace.sh
@@ -0,0 +1,6 @@
+export TORCH_LOGS=output_code 
+export TORCH_TRACE=/home/douliyang/large/mlsys/vllm-dly/examples/offline_inference/basic/trace_dir 
+python generate.py --model meta-llama/Meta-Llama-3-8B --tensor-parallel-size 2 --max-tokens 1024 --temperature 0.8 2>&1 | tee llama3-8B-debug.log
+
+# 解析日志
+tlparse trace_dir/dedicated_log_torch_trace_rank_0_hbg_vvrj.log    
